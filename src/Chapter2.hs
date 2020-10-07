0136395625c0ebfd6f19319414c378c09e105dab
Shinto C V <cshintov@gmail.com>
2020-10-07 18:31:15 +0530

    Solve chapter 2 (#2)

--- src/Chapter2.hs
+++ src/Chapter2.hs
@@ -136,42 +136,52 @@ functions in GHCi and insert the corresponding resulting output below:
 
 List of booleans:
 >>> :t [True, False]
+[True, False] :: [Bool]
 
 
 String is a list of characters:
 >>> :t "some string"
+"some string" :: [Char]
 
 
 Empty list:
 >>> :t []
+[] :: [a]
 
 
 Append two lists:
 >>> :t (++)
+(++) :: [a] -> [a] -> [a]
 
 
 Prepend an element at the beginning of a list:
 >>> :t (:)
+(:) :: a -> [a] -> [a]
 
 
 Reverse a list:
 >>> :t reverse
+reverse :: [a] -> [a]
 
 
 Take first N elements of a list:
 >>> :t take
+take :: Int -> [a] -> [a]
 
 
 Create list from N same elements:
 >>> :t replicate
+replicate :: Int -> a -> [a]
 
 
 Split a string by line breaks:
 >>> :t lines
+lines :: String -> [String]
 
 
 Join a list of strings with line breaks:
 >>> :t unlines
+unlines :: [String] -> String
 
 
 -}
@@ -186,31 +196,43 @@ Evaluate the following expressions in GHCi and insert the answers. Try
 to guess first, what you will see.
 
 >>> [10, 2] ++ [3, 1, 5]
+[10,2,3,1,5]
 
 >>> [] ++ [1, 4]  -- [] is an empty list
+[1,4]
 
 >>> 3 : [1, 2]
+[3,1,2]
 
 >>> 4 : 2 : [5, 10]  -- prepend multiple elements
+[4,2,5,10]
 
 >>> [1 .. 10]  -- list ranges
+[1,2,3,4,5,6,7,8,9,10]
 
 >>> [10 .. 1]
+[]
 
 >>> [10, 9 .. 1]  -- backwards list with explicit step
+[10,9,8,7,6,5,4,3,2,1]
 
 >>> length [4, 10, 5]  -- list length
+3
 
 >>> replicate 5 True
+[True,True,True,True,True]
 
 >>> take 5 "Hello, World!"
+"Hello"
 
 >>> drop 5 "Hello, World!"
+", World!"
 
 >>> zip "abc" [1, 2, 3]  -- convert two lists to a single list of pairs
+[('a',1),('b',2),('c',3)]
 
 >>> words "Hello   Haskell     World!"  -- split the string into the list of words
-
+["Hello","Haskell","World!"]
 
 
 👩‍🔬 Haskell has a lot of syntax sugar. In the case with lists, any
@@ -336,7 +358,10 @@ from it!
 ghci> :l src/Chapter2.hs
 -}
 subList :: Int -> Int -> [a] -> [a]
-subList = error "subList: Not implemented!"
+subList start end list = 
+    if (start < 0 ||  end < 0)
+        then []
+        else drop start (take (end + 1) list)
 
 {- |
 =⚔️= Task 4
@@ -349,8 +374,11 @@ Implement a function that returns only the first half of a given list.
 "b"
 -}
 -- PUT THE FUNCTION TYPE IN HERE
-firstHalf l = error "firstHalf: Not implemented!"
-
+firstHalf :: [a] -> [a]
+firstHalf l = 
+   take mid l
+   where
+     mid = div (length l) 2
 
 {- |
 =🛡= Pattern matching
@@ -500,9 +528,10 @@ True
 >>> isThird42 [42, 42, 0, 42]
 False
 -}
-isThird42 = error "isThird42: Not implemented!"
-
-
+isThird42 :: [Int] -> Bool
+isThird42 (_ : _ : 42 : _) = True
+isThird42 _ = False
+    
 {- |
 =🛡= Recursion
 
@@ -605,7 +634,8 @@ Implement a function that duplicates each element of the list
 
 -}
 duplicate :: [a] -> [a]
-duplicate = error "duplicate: Not implemented!"
+duplicate [] = []
+duplicate (x:xs) = x : x : duplicate xs 
 
 
 {- |
@@ -620,7 +650,25 @@ Write a function that takes elements of a list only on even positions.
 >>> takeEven [2, 1, 3, 5, 4]
 [2,3,4]
 -}
-takeEven = error "takeEven: Not implemented!"
+
+-- takeEven :: [a] -> [a]
+-- takeEven [] = [] 
+-- takeEven [x] = [x]
+-- takeEven (x:_:xs) = x : takeEven xs
+
+-- takeEven :: [a] -> [a]
+-- takeEven [] = [] 
+-- takeEven (x:xs) = x : takeEven (drop 1 xs)
+
+takeEven :: [a] -> [a]
+takeEven list = go list True
+    where 
+        go :: [a] -> Bool -> [a]
+        go [] _ = []
+        go (x:xs) isEven = 
+            if isEven 
+                then x : go xs False
+                else go xs True 
 
 {- |
 =🛡= Higher-order functions
@@ -651,7 +699,7 @@ the same way as any other values and expressions:
  ✲ And much more!
 
 The ability to create __lambdas__ (or anonymous functions) nicely
-complements the concept of HOF. For example, we can easily append
+complements the concept of HOF. For example, we can easily add
 number 3 to each element of the list by introducing a lambda function:
 
 >>> map (\x -> x + 3) [0..5]
@@ -665,7 +713,7 @@ specify space-separated arguments. Instead of the "=" in the ordinary
 function body, you should write "->" and then you can use these
 arguments and all variables in scope inside the lambda-body.
 
-There are equal:
+These are equal:
 
 @
 foo a b = a + b
@@ -727,7 +775,7 @@ value of the element itself
 🕯 HINT: Use combination of 'map' and 'replicate'
 -}
 smartReplicate :: [Int] -> [Int]
-smartReplicate l = error "smartReplicate: Not implemented!"
+smartReplicate = concatMap (\x -> replicate x x) 
 
 {- |
 =⚔️= Task 9
@@ -740,8 +788,8 @@ the list with only those lists that contain a passed element.
 
 🕯 HINT: Use the 'elem' function to check whether an element belongs to a list
 -}
-contains = error "contains: Not implemented!"
-
+contains :: Int -> [[Int]] -> [[Int]]
+contains n = filter (elem n)
 
 {- |
 =🛡= Eta-reduction
@@ -780,13 +828,15 @@ Let's now try to eta-reduce some of the functions and ensure that we
 mastered the skill of eta-reducing.
 -}
 divideTenBy :: Int -> Int
-divideTenBy x = div 10 x
+divideTenBy = div 10
 
 -- TODO: type ;)
-listElementsLessThan x l = filter (< x) l
+listElementsLessThan :: Int -> [Int] -> [Int]
+listElementsLessThan x = filter (< x)
 
 -- Can you eta-reduce this one???
-pairMul xs ys = zipWith (*) xs ys
+pairMul :: [Int] -> [Int] -> [Int]
+pairMul = zipWith (*)
 
 {- |
 =🛡= Lazy evaluation
@@ -841,7 +891,12 @@ list.
 
 🕯 HINT: Use the 'cycle' function
 -}
-rotate = error "rotate: Not implemented!"
+rotate :: Int -> [a] -> [a]
+rotate _ [] = []
+rotate n list =   
+    if n < 0 then [] 
+    else drop n (take (n + size) (cycle list))
+        where size = length list
 
 {- |
 =💣= Task 12*
@@ -857,10 +912,14 @@ and reverses it.
   function, but in this task, you need to implement it manually. No
   cheating!
 -}
-rewind = error "rewind: Not Implemented!"
 
+rewind :: [a] -> [a]
+rewind list = go list [] 
+    where 
+      go [] acc = acc
+      go (x:xs) acc = go xs (x:acc)
 
 {-
-You did it! Now it is time to the open pull request with your changes
+You did it! Now it is time to open a pull request with your changes
 and summon @vrom911 and @chshersh for the review!
 -}
